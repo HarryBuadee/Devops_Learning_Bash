e2e4174c8fd0792d4e26c8f672ae1aa77e8e29ac
HarryBuadee <158767433+HarryBuadee@users.noreply.github.com>
2026-01-02 12:06:15 +0000

    Update Level-1-the-basics.sh

--- Bash_Scripting/Bash Assignment/Bash_battle_arena/Level-1-the-basics.sh
+++ Bash_Scripting/Bash Assignment/Bash_battle_arena/Level-1-the-basics.sh
@@ -1,6 +1,5 @@
 
 # Mission: Create a directory named Arena and then inside it, create three files: warrior.txt, mage.txt, and archer.txt. List the contents of the Arena directory.
-```` 
 #!/bin/bash
 
 mkdir Arena
@@ -8,4 +7,4 @@ touch warrior.txt mage.txt archer.txt
 mv warrior.txt mage.txt archer.txt arena/
 ls Arena
 archer.txt  mage.txt  warrior.txt
-`````
+

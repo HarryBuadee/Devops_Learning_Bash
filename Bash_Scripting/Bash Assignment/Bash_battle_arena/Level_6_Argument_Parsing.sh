5869320fe22538dfba6fa2a465a6fc4d72da9e89
HarryBuadee <158767433+HarryBuadee@users.noreply.github.com>
2026-01-03 15:24:15 +0000

    Update Level_6_Argument_Parsing.sh

--- Bash_Scripting/Bash Assignment/Bash_battle_arena/Level_6_Argument_Parsing.sh
+++ Bash_Scripting/Bash Assignment/Bash_battle_arena/Level_6_Argument_Parsing.sh
@@ -2,9 +2,6 @@
 
 #Mission: Write a script that accepts a filename as an argument and prints the number of lines in that file. If no filename is provided, display a message saying 'No file provided'.
 
-
-
-
 filename="$1"
 
 if [ -z "$filename" ]; then
